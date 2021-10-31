580f95899557578774f91c602aa9ffd7b7e360fe
koderhan <koderhanov@gmail.com>
2021-10-31 16:32:57 +0300

    first

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,13 @@
   * @param {number} first - первое число
   * @returns {number}
   */
+
+ jQuery(function($) {
+  $('input[type="file"]').on('change', function() {
+    console.log('okeee')
+    if ($(this).val()) {
+         var filename = $(this).val();
+         $('.file-name').html(filename);
+    }
+  });
+});
