725fb605d3d878ef341940cf5600016f9c1839c1
Ramil Khusyainov <koderhanov@gmail.com>
2021-11-01 19:59:03 +0300

    Update 2021-11-01T16:58:32.742Z

---
+++ js/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+  * название функции
+  *
+  * @param {number} first - первое число
+  * @returns {number}
+  */
+jQuery(function ($) {
+  $('input[type="file"]').on('change', function () {
+    console.log('okeee');
+
+    if ($(this).val()) {
+      var filename = $(this).val();
+      $('.file-name').html(filename);
+    }
+  });
+});
+//# sourceMappingURL=main.js.map
